5893d4ca6fe27fd923dac6f5e2cdb02ee5a22af8
samsanimahesh <samsanimahesh@gmail.com>
2024-05-25 11:58:01 -0500

    spotify mock initialized

---
+++ src/main/java/org/mock/spotify/spotify_mock/SpotifyMockApplication.java
@@ -0,0 +1,13 @@
+package org.mock.spotify.spotify_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mock/spotify/spotify_mock/SpotifyMockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mock.spotify.spotify_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
